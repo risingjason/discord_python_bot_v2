1618545d60029f5a8473975ad7d9863e9d2a09ad
Jason Zhang <risingjason@gmail.com>
2017-08-14 20:41:39 -0700

    error checking for connect4

--- central.py
+++ central.py
@@ -23,6 +23,7 @@ online_message = "`Bot v2.0 Currently in Development`"
 
 # added text channels filter list to avoid spamming multiple text channels
 allowed_text_channels = ["93456386729451520", "169666525404463105"]
+master_user = "91115380646354944"
 
 @client.event
 async def on_ready():
@@ -43,10 +44,11 @@ async def on_message(msg):
 	cmds = [x.lower() for x in msg.content.split(" ")] # parses string and makes them lowercase
 	cmd = cmds[0] # first word is always !command; used to access commands.py module
 	mentions = msg.raw_mentions # might be of use in the future
-
+	# print(cmds)
 	#waits for any of the commands in commands.py to be called upon
 	#if msg.channel.id in allowed_text_channels:
-	if cmd in commands.commands:
+
+	if cmd in commands.commands and msg.author.id == master_user :
 		await commands.commands[cmd](client, msg, cmds)
 
 print("Starting bot...")
--- commands.py
+++ commands.py
@@ -3,17 +3,28 @@ import discord
 connect_four_sessions = {}
 users_in_c4_session = []
 
+connect_four_rules = "`Connect4! Start game with !connect4 start @user" +\
+                     "- start: the person who types the message is player 1 and @user is player 2" +\
+                     "- play: !connect4 place (column) to place a piece in a column from 1-7" +\
+                     "- if @user is this bot, then you will play against an AI (currently in development)`"
+
 async def cmd_connect_four(client, msg, cmds):
-    # correct command looks like this: "!connect4 start @user"
+    # correct command looks like this: "!connect4 start @user" ex. user mention string <@153142506014507008>
     # @user can any human, if @user is the bot, then play against AI
     # only one player can be in a session at a time
-    #if not connect_four_sessions:
-    #    if len(cmds) != 3:
-    #        await client.send_message(msg.channel, "`Invalid Command. Example command: !connect4 start @user`")
-    #    elif cmds[0]
+    #print(cmds[2][2:-1])
+    if len(cmds) == 1:
+        await client.send_message(msg.channel, connect_four_rules)
+        return
+
+    if not connect_four_sessions:
+        if len(cmds) != 3:
+            await client.send_message(msg.channel, "`Invalid Command. Example command: !connect4 start @user`")
+        
+        if cmds[1] == "start" and cmds[2][2:-1] in users_in_c4_session:
+            await client.send_message(msg.channel, "")
 
     # placing tile command: "!connect4 place X" where X is a column from 1-7
-
 
 def emoji_board(board):
     # prints connect four game board in emoji form
