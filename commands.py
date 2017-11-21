607c2e97c78b60b7e568a27f43626242016fe35a
Jason Zhang <jzhang5137@gmail.com>
2017-11-21 04:11:14 -0800

    added .gitignore for token and updated some import names

--- commands.py
+++ commands.py
@@ -1,8 +1,8 @@
 import discord
-import connect4_play
+import c4_play
 
 async def cmd_connect_four(client, msg, cmds):
-    await connect4_play.connect_four(client, msg, cmds)
+    await c4_play.connect_four(client, msg, cmds)
     return
 
 commands = { "!c4":cmd_connect_four, "!connect4":cmd_connect_four }
